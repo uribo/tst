3ddfc7afbd99491018d91160f2bc52c49a54af5f
Shinya Uryu <r.sh1nya.uryu@gmail.com>
2018-03-21 10:38:58 +0900

    add `expect_dimension` 🌟

--- R/expect_geometry.R
+++ R/expect_geometry.R
@@ -1,11 +1,18 @@
 #' Expect geometry type
 #'
+#' @details
+#'
+#' * `expect_geom_type()`: test with which geometry type
+#' * `expect_dimention()`: test with N's spatial dimension of geometry. Expect values are 0 for points,
+#' 1 for lines, 2 for planar.
+#'
 #' @import sf
 #' @import testthat
 #' @importFrom rlang enquo
 #'
 #' @param object object of class \code{\link[sf]{sf}} or \code{\link[sf]{sfc}}
 #' @param type test equality geometry type (`character`)
+#' @param value expected value
 #'
 #' @name expect_geometry
 #' @examples
@@ -33,6 +40,25 @@ expect_geom_type <- function(object,
       x = act$lab,
       y = act$type,
       z = type
+    )
+  )
+
+  invisible(act$val)
+}
+
+#' @rdname expect_geometry
+#' @export
+expect_dimension <- function(object, value) {
+  act <- testthat::quasi_label(rlang::enquo(object))
+
+  act$value <- as.character(sf::st_dimension(act$val))
+  testthat::expect(
+    act$value == value,
+    glue::glue(
+      "{x}, dimension of geometry is {y}, not {z}.",
+      x = act$lab,
+      y = act$value,
+      z = value
     )
   )
 
--- tests/testthat/test-geometry.R
+++ tests/testthat/test-geometry.R
@@ -17,16 +17,9 @@ test_that("geom-type", {
                                  "LINESTRING"))
 })
 
-  expect_silent(
-    expect_geom_type(
-      c(sf::st_point(1:2), sf::st_point(5:6)),
-      "MULTIPOINT"
-    )
-  )
-  expect_silent(
-    expect_geom_type(
-      sf::st_polygon(list(rbind(c(0, 0), c(1, 0), c(1, 1), c(0, 1), c(0, 0)))), # nolint
-      "POLYGON"
-    )
-  )
+test_that("dimension", {
+  expect_silent(expect_dimension(sf_point, 0))
+  expect_silent(expect_dimension(sf_point, "0"))
+  expect_silent(expect_dimension(sf_line,
+                                 1))
 })
