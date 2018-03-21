a890ae27123b02b8caf6d5cc186cfe8f7d7fd480
Shinya Uryu <r.sh1nya.uryu@gmail.com>
2018-03-21 10:18:02 +0900

    tweaked

--- tests/testthat/test-geometry.R
+++ tests/testthat/test-geometry.R
@@ -1,19 +1,21 @@
 context("test-geometry.R")
 
-x <- st_point(c(0, 0))
+sf_point <- st_point(c(0, 0))
+sf_line <- st_linestring(matrix(1:12, ncol = 3))
 
-test_that("multiplication works", {
-  expect_silent(
-    expect_geom_type(x, type = "POINT")
-  )
-
-  expect_error(
-    expect_geom_type(x, type = "LINESTRING")
-  )
-
-  expect_error(
-    expect_geom_type(x, type = "LINE")
-  )
+test_that("geom-type", {
+  expect_silent(expect_geom_type(sf_point, type = "POINT"))
+  expect_error(expect_geom_type(sf_point, type = "LINESTRING"))
+  expect_error(expect_geom_type(sf_point, type = "LINE"))
+  expect_silent(expect_geom_type(c(sf::st_point(1:2), sf::st_point(5:6)),
+                                 "MULTIPOINT"))
+  expect_silent(expect_geom_type(sf::st_polygon(list(
+    rbind(c(0, 0), c(1, 0), c(1, 1), c(0, 1), c(0, 0))
+  )), # nolint
+  "POLYGON"))
+  expect_silent(expect_geom_type(sf_line,
+                                 "LINESTRING"))
+})
 
   expect_silent(
     expect_geom_type(
